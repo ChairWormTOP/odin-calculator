6ed1e16607326677cda2d9fb3af148bd2c11b18f
ChairWormTOP <chairworm@hotmail.com>
2022-06-18 22:49:24 -0400

    Add backspace functionality

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ const backspace = document.querySelector('.backspace')
 
 const setDisplay = (value) => {
     display.textContent += value
+    
 }
 
 const clearDisplay = () => {
-    display.textContent = ''
+    display.textContent
+}
+
+const backSpace = () => {
+    display.textContent = display.textContent.slice(0, display.textContent.length - 1)
 }
 
 numbers.forEach(item => item.addEventListener('click', () => setDisplay(item.textContent)))
 
 clear.addEventListener('click', () => clearDisplay())
+
+backspace.addEventListener('click', () => backSpace())
